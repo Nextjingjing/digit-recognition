b6720ae8b6c53db991fc2f59dbddbfbf81a27f9a
SNeXT <snext2004@gmail.com>
2025-05-18 23:17:08 +0700

    finish project

--- app/main.py
+++ app/main.py
@@ -1,5 +1,10 @@
 import tkinter as tk
 
+from ui.Canvas import Canvas
+
 if __name__ == "__main__":
     root = tk.Tk()
+    root.geometry("200x250")
+    canvas_widget = Canvas(root)
+    canvas_widget.get_frame().pack(expand=True, fill=tk.BOTH)
     root.mainloop()
--- app/ui/Canvas.py
+++ app/ui/Canvas.py
@@ -1,12 +1,16 @@
 import tkinter as tk
 import numpy as np
 from PIL import Image, ImageDraw
+import joblib
+import os
 
 class Canvas:
     def __init__(self, root):
-        self._frame = tk.Frame(root, bg="#cccccc")
+        self._frame = tk.Frame(root, bg="#cccccc", bd=3, relief="ridge")
         self._image = np.zeros((8, 8), dtype=np.uint8)
         self._create_ui()
+        self._load_model()
+        
 
     def get_image(self):
         return self._image
@@ -24,8 +28,14 @@ class Canvas:
         btn_clear = tk.Button(self._frame, text="Clear", command=self._clear_canvas)
         btn_clear.pack(pady=10)
 
-        btn_draw = tk.Button(self._frame, text="Predict", command=self.draw_image)
+        btn_draw = tk.Button(self._frame, text="Predict", command=self._predict)
         btn_draw.pack(pady=5)
+
+        self._pred_var = tk.StringVar()
+        self._pred_var.set("Predict: Null")
+
+        pred_label = tk.Label(self._frame, textvariable=self._pred_var)
+        pred_label.pack()
 
     def _on_click(self, event):
         self._last_x, self._last_y = event.x, event.y
@@ -38,6 +48,7 @@ class Canvas:
 
     def _clear_canvas(self):
         self._canvas.delete("all")
+        self._pred_var.set("Predict: Null")
 
     def _draw_line(self, img, x0, y0, x1, y1):
         pil_img = Image.fromarray(img)
@@ -45,7 +56,7 @@ class Canvas:
         draw.line((x0, y0, x1, y1), fill=255, width=2)
         np.copyto(img, np.array(pil_img, dtype=np.uint8))
 
-    def draw_image(self):
+    def _predict(self):
         img = np.zeros((80, 80), dtype=np.uint8)
 
         for item in self._canvas.find_all():
@@ -64,12 +75,18 @@ class Canvas:
                 image8x8[i, j] = int(avg_intensity / 255 * 15)
 
         self._image = image8x8
+        pred = self._model.predict(self._image.reshape(1, -1))[0]
+        self._pred_var.set(f"Predict: {pred}")
 
-        self._canvas.delete("all")
+    def _load_model(self):
+        base_dir = os.path.dirname(os.path.abspath(__file__))
+        model_path = os.path.join(base_dir, "..", "model", "digit_recognizer_model.pkl")
+        self._model = joblib.load(model_path)
+
 
 if __name__ == "__main__":
     root = tk.Tk()
-    root.geometry("200x300")
+    root.geometry("175x200")
     canvas_widget = Canvas(root)
     canvas_widget.get_frame().pack()
     root.mainloop()
